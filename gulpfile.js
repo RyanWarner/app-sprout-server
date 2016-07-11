dc17b0f409e03733d859d06810395d306f1c5d11
Ryan Warner <warner.ryan@gmail.com>
2016-07-11 10:26:43 -0700

    lint gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,67 @@
-var gulp 		= require( 'gulp' );
+var gulp 		= require('gulp');
+var gutil 		= require('gulp-util');
+var runSequence = require('run-sequence');
 
-var gutil 		= require( 'gulp-util' );
-var runSequence = require( 'run-sequence' );
-var nodemon 	= require( 'gulp-nodemon' );
+var nodemon 	= require('gulp-nodemon');
+var express 	= require('express');
+var app 		= express();
 
-var express 	= require( 'express' );
-var app 		= express(  );
-
-var eslint      = require( 'gulp-eslint' );
-
-var mocha       = require( 'gulp-spawn-mocha' );
+var eslint      = require('gulp-eslint');
+var mocha       = require('gulp-spawn-mocha');
 
 
 var SCRIPTS = 'server/**/*.js';
-var TESTS = 'tests/**/*.js';
+var TESTS   = 'tests/**/*.js';
 
-var onError = function ( err )
-{  
-	gutil.beep(  );
-	console.log( err );
+var onError = function(err) {  
+	gutil.beep();
+	console.log(err);
 };
 
-gulp.task( 'watch', function(  )
-{
-    gulp.watch( [ SCRIPTS, TESTS ], [ 'eslint' ] );
-} );
+
+
+gulp.task('watch', function() {
+    gulp.watch([SCRIPTS, TESTS], ['eslint']);
+});
 
 
 
-gulp.task( 'eslint', function(  )
-{
+gulp.task('eslint', function() {
     return gulp
-   		.src( [ SCRIPTS, TESTS ] )
-        .pipe( eslint(  ) )
-        .pipe( eslint.format(  ) );
-} );
+   		.src([SCRIPTS, TESTS])
+        .pipe(eslint())
+        .pipe(eslint.format());
+});
 
 
-gulp.task( 'tests', function(  )
-{
+
+gulp.task('tests', function() {
 	return gulp
-	    .src( TESTS, { read: false } )
-	    .pipe( mocha( {
+	    .src(TESTS, { read: false })
+	    .pipe(mocha({
 
 			env:
 			{
 		   		'NODE_ENV': 'test'
 			}
-
-	    } ) );
-} );
-
-gulp.task( 'serve', function(  )
-{
-	nodemon(
-	{
-		script: 'server.js'
-	} );
-	gutil.log( 'Listening.' );
+	    }));
 });
 
 
 
-gulp.task( 'default', function(  )
-{
+gulp.task('serve', function() {
+	nodemon({
+		script: 'server.js'
+	});
+	gutil.log('Listening.');
+});
+
+
+
+gulp.task('default', function() {
 	runSequence(
 		'eslint',
 		'watch',
 		'serve'
 	);
-} );
+});
